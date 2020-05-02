66bba471a9ea656180e0aebc09435ded4e2f518a
raduceaca1234 <ceaca.radu@yahoo.com>
2020-05-02 23:23:13 +0300

    added lab 9

---
+++ lab9mpp/common/src/main/java/service/ClientService.java
@@ -0,0 +1,18 @@
+package service;
+
+import domain.Client;
+import domain.exceptions.ValidatorException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface ClientService extends Service<Long, Client> {
+    void addClient(Client entity) throws ValidatorException, IOException;
+    List<Client> getAllClients(String... sort);
+    List<Client> filterClientsByFirstName(String name);
+    List<Client> filterClientsByLastName(String name);
+    List<Client> filterClientsByAge(int age);
+    void removeClient(Long id);
+    void updateClient(Client entity);
+}
